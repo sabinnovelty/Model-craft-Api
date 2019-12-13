53a5ccfc96a72f8177e3f42eda65aee2805b07a5
novelty <sabin@noveltytechnology.com>
2019-12-13 21:20:46 +0545

    heroku

--- route.js
+++ route.js
@@ -3,7 +3,7 @@ const authController = require("./controllers/auth-controller");
 const eventController = require("./controllers/event-controller");
 const serviceController = require('./controllers/service-controller');
 
-router.use('/', (req, res) => {
+router.get('/', (req, res) => {
     res.json({
         message: "Welcome to model craft Server"
     })
